34be7a47073f44169ac8e606ba790d5ef8f800f6
Teodor Siustis <siustis.teodor@gmail.com>
2023-04-22 20:10:39 +0300

    Initial commit for frontend part

---
+++ online-shop/src/App.jsx
@@ -0,0 +1,15 @@
+
+import HomePage from './components/HomePage'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+      <HomePage/>
+    </>
+  )
+}
+
+export default App
